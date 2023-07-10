efe900a468af93371427e28b7affcbbefbae09a3
kamikaonashi <thorschi93@gmx.at>
2023-07-10 11:40:24 +0200

    mido: adapt for AlphaDroid

--- lineage_mido.mk
+++ lineage_mido.mk
@@ -21,8 +21,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common Alphadroid stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Gapps Flags
+WITH_GAPPS := 2
+TARGET_USE_GOOGLE_TELEPHONY := true
+TARGET_USE_PIXEL_FRAMEWORK := true
+
+# Maintainer Flags
+ALPHA_MAINTAINER := KamiKaonashi
+ALPHA_BUILD_TYPE := Official
+
+# Debugging Flags
+TARGET_INCLUDE_MATLOG := false
+TARGET_DEFAULT_ADB_ENABLED := false
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
